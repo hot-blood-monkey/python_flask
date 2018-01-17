c069b2a8b51353a51f158454e3511e083e52822f
zhouhao_HN <290322402@qq.com>
2018-01-17 15:23:23 +0800

    11h

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template,session,redirect,url_for,current_app,abort,flash
+from flask import render_template,session,redirect,url_for,current_app,abort,flash,request
 from flask_login import login_required,current_user
 
 from ..email import send_email
@@ -19,8 +19,14 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+
+    #添加分页显示博客
+    page = request.args.get('page',1,type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+                page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+                error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
 
 
@@ -30,6 +36,16 @@ def index():
 #     if user is None:
 #         abort(404)
 #     return render_template('user.html',user=user)
+
+
+
+@main.route('/user/<username>')
+def user(username):
+    user=User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    posts=user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html',user=user,posts=posts)
 
 
 @main.route('/edit-profile',methods=['GET','POST'])
@@ -49,7 +65,7 @@ def edit_profile():
     return render_template('edit_profile.html',form=form)
 
 
-@main.route('/edit-profile/<int:id>',methods=['GET','POST'])
+@main.route('/edit-profile/<int:id>',methods=['GET', 'POST'])
 @login_required
 @admin_required
 def edit_profile_admin(id):
@@ -75,15 +91,25 @@ def edit_profile_admin(id):
     form.about_me = user.about_me
     return render_template('edit_profile.html',form=form,user=user)
 
+@main.route('/post/<int:id>')
+def post(id):           #文章的固定连接
+    post = Post.query.get_or_404(id)
+    return render_template('post.html',posts=[post])
 
-@main.route('/user/<username>')
-def user(username):
-    user=User.query.filter_by(username=username).first()
-    if user is None:
+@main.route('/edit/<int:id>',methods=['GET','POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and not current_user.can(Permission.ADMINSTER):
         abort(404)
-    posts=user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html',user=user,posts=posts)
-
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('这篇文章已经更新了')
+        return redirect(url_for('post',id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html',form=form)
 
 
 
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import os,hashlib
 from datetime import datetime
+from markdown import markdown
+import bleach
 from . import db
 from . import login_manager
 from config import config
@@ -23,16 +25,47 @@ class Post(db.Model):
     body=db.Column(db.Text)
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
     author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed,randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+
+        for i in range(count):
+            u=User.query.offset(randint(0,user_count - 1)).first()
+            p=Post(
+                body=forgery_py.lorem_ipsum.sentences(randint(1,3)),
+                timestamp = forgery_py.date.date(True),
+                author=u
+            )
+
+            db.session.add(p)
+            db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target,value,oldvalue,initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+                             markdown(value, output_format='html'),
+                             tags=allowed_tags, strip=True ))
+
+db.event.listen(Post.body,'set',Post.on_changed_body)
 
 
 class Role(db.Model):
     __tablename__='roles'
-    id =db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(32),unique=True)
-    default=db.Column(db.Boolean,default=False,index=True)
+    id =db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(32), unique=True)
+    default=db.Column(db.Boolean,default=False, index=True)
     permissions = db.Column(db.Integer)
 
-    users=db.relationship('User',backref='role',lazy='dynamic')
+    users=db.relationship('User', backref='role', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(Role, self).__init__(**kwargs)
@@ -111,6 +144,22 @@ class User(UserMixin,db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
 
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed,randint
+        import forgery_py
+
+        seed()
+        user_count =User.query.count()
+
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count-1)).first()
+            p=Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
+                   timestamp=forgery_py.date.date(True),
+                   author=u
+                   )
+            db.session.add(p)
+            db.session.commit()
 
 
     def ping(self):
